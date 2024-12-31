c7e3e4373691dcee369c6dcf40587777ee40f110
wanyaxing <wyx@wanyaxing.com>
2024-12-31 15:40:04 +0800

    调整默认遍历逻辑，由仿list改为仿dict
    新增 update /sort/rename方法

--- dotquery/dotres.py
+++ dotquery/dotres.py
@@ -53,10 +53,13 @@ class DotRes:
     def __len__(self):
         return len(self._value)
 
-    # 支持遍历
-    # 统一效果，仿dict，即list遍历默认也是出index
+    # 支持遍历,仿list(即dict也是默认返回值集合，方便遍历，和dict本身行为不一样哦)
     def __iter__(self):
-        return self.keys()
+        return self.values()
+        # if isinstance(self._value, list):
+            # return self.values()
+        # elif isinstance(self._value, dict):
+        #     return self.keys()
 
     # 可以出key,value队
     def items(self):
@@ -82,6 +85,61 @@ class DotRes:
         elif isinstance(self._value, dict):
             for key in self._value:
                 yield self[key]
+
+
+    # 仿dict.update,但此处需要指定key，且只有key对应的值一致才会更新
+    def update(self, values=[],key='id'):
+        if isinstance(values, DotRes):
+            values=values._value
+        if isinstance(values, list):
+            return self._update_with_key(values,key)
+        elif isinstance(values, dict):
+            return self._update_with_key([values],key)
+
+
+    def _update_with_key(self, values=[],key='id'):
+        for val in values:
+            # if not isinstance(val, list):
+            #     raise ValueError('此处只接受字典用于合并数据')
+            # if key not in val:
+            #     raise ValueError('未在待合并字典中找到对应的key')
+            if isinstance(self._value, list):
+                isupdated = False
+                for i in range(len(self._value)):
+                    if key in self._value[i] and self._value[i][key]==val[key]:
+                        isupdated=True
+                        self._value[i].update(val)
+                if not isupdated:
+                    self._value.append(val)
+                    # todo 此处需考虑字段合并
+            elif isinstance(self._value, dict):
+                if self[key]==val[key]:
+                    self._value.update(val)
+
+    # 当值为数组时，指定其中的字典值进行排序
+    def sort(self, key='id', reverse=False):
+        if isinstance(self._value, list):
+            self._value=sorted(self._value, key=lambda x: x[key] if key in x else 0, reverse=reverse)
+            return self
+        elif isinstance(self._value, dict):
+            raise ValueError('此处只接受数组结果进行排序')
+
+    # 将字典中的key值重命名，可以用 .rename('from','to') 或 .rename({'f1':'t1','f2':'t2'})
+    def rename(self, *args):
+        keylist = {}
+        if len(args)==2 and isinstance(args[0], str) and isinstance(args[1], str):
+            keylist[args[0]]=args[1]
+        else:
+            for arg in args:
+                if type(arg) is dict:
+                    keylist.update(arg)
+        if isinstance(self._value, list):
+            for i in range(len(self._value)):
+                self._value[i]={keylist[old_key] if old_key in keylist else old_key: value for old_key, value in self._value[i].items()}
+        elif isinstance(self._value, dict):
+            self._value={keylist[old_key] if old_key in keylist else old_key: value for old_key, value in self._value.items()}
+        return self
+
 
     # 给当前对象下的对象，都指定默认值（包括错误调用（不含当前调用）））
     # 指定vin值，当查询结果的DotRes被外界调用时，此处可以指定默认值
